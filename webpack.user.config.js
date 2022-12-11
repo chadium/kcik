fd69c0601facda237a473c7b3bc2f549e5f45dd4
Daniel Araujo <contact@daniel-araujo.pt>
2022-12-11 14:10:15 +0000

    Add obfuscator to user production build

--- webpack.user.config.js
+++ webpack.user.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const { DefinePlugin } = require('webpack')
 const configs = require('./webpack.base.config.js')
+const WebpackObfuscator = require('webpack-obfuscator');
 
 for (let config of configs) {
   config.plugins.push(
@@ -8,6 +9,14 @@ for (let config of configs) {
       ADMIN: false
     })
   )
+
+  if (config.mode === 'production') {
+    config.plugins.push(
+      new WebpackObfuscator({
+        stringArray: true
+      })
+    )
+  }
 
   config.output.path = config.output.path.replace('[REPLACE]', path.join(__dirname, 'dist', 'user'))
 }
