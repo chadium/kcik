909a9b60ce3ebdcee774ac403dbfc5dfc13dc976
Daniel Araujo <contact@daniel-araujo.pt>
2022-12-15 20:51:38 +0000

    Add mmss and add representation for invalid

--- src/preload/duration-format.mjs
+++ src/preload/duration-format.mjs
@@ -1,4 +1,8 @@
 export function hhmmss(duration) {
+  if (!(duration >= 0)) {
+    return '--:--:--'
+  }
+
   const hours = Math.floor((duration / (1000 * 60 * 60)))
   const minutes = Math.floor((duration / 1000 / 60) % 60)
   const seconds = Math.floor((duration / 1000) % 60)
@@ -6,7 +10,22 @@ export function hhmmss(duration) {
   return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
 }
 
+export function mmss(duration) {
+  if (!(duration >= 0)) {
+    return '--:--'
+  }
+
+  const minutes = Math.floor((duration / 1000 / 60) % 60)
+  const seconds = Math.floor((duration / 1000) % 60)
+
+  return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
+}
+
 export function seconds(duration) {
+  if (!(duration >= 0)) {
+    return '--'
+  }
+
   const seconds = Math.floor((duration / 1000) % 60)
 
   return `${seconds.toString().padStart(2, '0')}`
