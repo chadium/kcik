ef0263c90ed3846e0b4f1d1616927d936ab3dd79
Daniel Araujo <contact@daniel-araujo.pt>
2023-05-10 14:01:27 +0100

    Talk about these great persons

--- src/chrome-popup/components/tabs/TabCredits.jsx
+++ src/chrome-popup/components/tabs/TabCredits.jsx
@@ -8,20 +8,32 @@ export default function TabCredits({ com }) {
   let { data, loading, error } = useResource(userApi.credits)
 
   return (
-    <GenericLoading loading={loading} error={error}>
-      {data && (
-        <div>
-          {data.kickSubscribers.map(username => (
-            <div key={username}>
-              {username}
-            </div>
-          ))}
-        </div>
-      )}
+    <div>
+      <div>
+        THESE WONDERFUL PEOPLE HAVE
+        MADE THE DEVELOPMENT OF
+        THIS EXTENSION POSSIBLE
+      </div>
+
+      <div className="boomer-p-t"/>
+
+      <GenericLoading loading={loading} error={error}>
+        {data && (
+          <div>
+            {data.kickSubscribers.map(username => (
+              <div key={username}>
+                {username}
+              </div>
+            ))}
+          </div>
+        )}
+      </GenericLoading>
+
+      <div className="boomer-p-t"/>
 
       <div className="text-center">
         Check <TextLink newTab={true} url="https://chadium.dev">https://chadium.dev</TextLink>
       </div>
-    </GenericLoading>
+    </div>
   )
 }
