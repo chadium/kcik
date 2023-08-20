66730885bbdb481782a7bd24033f16f9cb40a44c
Daniel Araujo <contact@daniel-araujo.pt>
2023-08-20 11:39:20 +0100

    Add mouse volume control to changelog

--- src/chrome-popup/components/tabs/TabHome.jsx
+++ src/chrome-popup/components/tabs/TabHome.jsx
@@ -13,6 +13,9 @@ export default function TabHome({ com, repo }) {
 
       <p>
         <b>Changelog</b>:<br/>
+        - <span className="chad-subtle">2023-08-20</span>: Mouse volume control.
+        You can now unmute streams with middle mouse button clicks and change
+        the volume with the scroll wheel.<br/>
         - <span className="chad-subtle">2023-08-06</span>: Website theme presets.<br/>
         - <span className="chad-subtle">2023-07-30</span>: You can now display deleted messages.<br/>
         - <span className="chad-subtle">2023-07-16</span>: You can now remove streamers from the
