c87eaa11d65cff13b574569cf7e3a6c90fa1c8e2
Daniel Araujo <contact@daniel-araujo.pt>
2023-10-09 20:31:24 +0000

    Add KCIK prefix to logs

--- src/preload/log.mjs
+++ src/preload/log.mjs
@@ -1,11 +1,11 @@
 export function info(tag, ...args) {
-  console.log(`%c ${tag} `, 'background: #dcff6c; color: #222222', ...args);
+  console.log(`%c KCIK:${tag} `, 'background: #dcff6c; color: #222222', ...args);
 }
 
 export function warn(tag, ...args) {
-  console.log(`%c ${tag} `, 'background: #ffe96c; color: #222222', ...args);
+  console.log(`%c KCIK:${tag} `, 'background: #ffe96c; color: #222222', ...args);
 }
 
 export function bad(tag, ...args) {
-  console.error(`%c ${tag} `, 'background: #ff6c6c; color: #ffffff', ...args);
+  console.error(`%c KCIK:${tag} `, 'background: #ff6c6c; color: #ffffff', ...args);
 }
