da254ae7de629bed30bb34475116b23e31014a37
Daniel Araujo <contact@daniel-araujo.pt>
2023-11-12 12:48:13 +0000

    Update home

--- src/chrome-popup/components/tabs/TabHome.jsx
+++ src/chrome-popup/components/tabs/TabHome.jsx
@@ -5,14 +5,16 @@ export default function TabHome({ com, repo }) {
   return (
     <div>
       <p>
-        Use the navigation buttons up top to configure the features
-        of this extension to your liking.
+        Use the navigation buttons up top to configure this extension
+        to your liking.
       </p>
 
       <div className="chad-p-t"/>
 
       <p>
         <b>Changelog</b>:<br/>
+        - <span className="chad-subtle">2023-11-11</span>: Hide streamers in the
+        sidebar.<br/>
         - <span className="chad-subtle">2023-10-10</span>: Send message history!
         Press up to get your previous message.<br/>
         - <span className="chad-subtle">2023-09-16</span>: Sidebar stream tooltip.
